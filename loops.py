96e4b145bc5cc3e724f90e1d8a428c5324a2a8d4
Matumbai Kevin <matushkevin@gmail.com>
2026-02-05 20:49:33 +0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,9 +1,19 @@
 #Print numbers 1–10 using a for loop.
-
+for i in range(1, 11, 1):
+    print(i)
 #Print numbers 10–1 using a while loop.
-
+count = 10
+while count >= 1:
+    print(count)
+    count -= 1
 #Ask for a number and print its multiplication table.
-
+num = int(input("Provide a number: "))
+for i in range(1, 11):
+    print(num, "x", i, "=", num * i)
 #Print only even numbers between 1 and 50.
-
+for Num in range(1, 50):
+    if Num % 2 == 0:
+        print(Num)
 #Loop through a name and print each character.
+for u in "banana":
+    print(u)
