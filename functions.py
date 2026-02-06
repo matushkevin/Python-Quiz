aba71aa02cd776b38a6c79f594ca60c9186fb64d
Matumbai Kevin <matushkevin@gmail.com>
2026-02-06 06:46:48 +0300

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,24 @@
+#Write a function that prints a welcome message.
+def greet():
+    print("Welcome to this page.")
+greet()
+#Write a function that takes a name and prints it.
+def print_name(name):
+    print(name)
+#Write a function that adds two numbers and returns the result.
+def add(x, y):
+    return x + y
+result = add(5, 8)
+print(result)
+#Write a function that checks if a number is positive or negative.
+def check(z):
+    if z >0:
+        print("Positive")
+    elif z < 0:
+        print("Negative")
+    else:
+        print("Zero")
+#Write a function that takes name and age and prints a profile.
+def profile(name, age):
+    print("Name:", name)
+    print("Age:", age)
